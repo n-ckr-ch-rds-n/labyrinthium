671a1397d08cd6c0dc7b61ca38132883b0ed34b4
n-ckr-ch-rds-n <nicholasjohnrichardson@gmail.com>
2022-12-15 10:10:44 +0000

    Delete unused const

--- src/colour.by.square.kind.ts
+++
@@ -1,8 +0,0 @@
-import { SquareKind } from "./square.kind";
-
-export const colourBySquareKind: Record<SquareKind, string> = {
-    [SquareKind.Path]: 'black',
-    [SquareKind.Wall]: 'green',
-    [SquareKind.End]: 'orange',
-    [SquareKind.Start]: 'pink'
-}
