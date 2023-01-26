f72c0cdb6201a1b2ef9e6c69183ed998b3571c7d
n-ckr-ch-rds-n <nicholasjohnrichardson@gmail.com>
2023-01-26 15:14:31 +0000

    Tidy up app component

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,17 @@
-<script lang="ts">
-    import { Wanderer } from "./wanderer";
-	import { Maze } from "./maze";
-    import { MovementService } from "./movement.service";
-    import { DrawService } from "./draw.service";    
+<script lang="ts">  
     import Game from "./Game.svelte";
     import type { InitConfig } from "./init.config";
 
 	const complexity = 300;
-	const gameArea: HTMLCanvasElement = document.createElement('canvas');
 	const squareWidth = Math.floor(window.innerWidth / complexity);
-	gameArea.width = squareWidth * complexity;
-	const canvasWidth = squareWidth * complexity;
 	const numberOfRows = Math.floor(window.innerHeight / squareWidth);
-    gameArea.height = squareWidth * numberOfRows;
-	const canvasHeight = squareWidth * numberOfRows;
-	const container = document.createElement('div');
-	container.style.cssText += 'display:flex;justify-content:center;';
-	gameArea.style.cssText += 'border: 2px solid black;';
-	container.appendChild(gameArea);
-	const canvas = gameArea;
 	const initConfig: InitConfig = {
 		squareWidth,
-		canvasHeight,
-		canvasWidth,
+		canvasHeight: squareWidth * numberOfRows,
+		canvasWidth: squareWidth * complexity,
 		numberOfColumns: complexity,
 		numberOfRows
 	}
-	// document.body.insertBefore(container, document.body.childNodes[0]);
-    const gameContext: CanvasRenderingContext2D = gameArea.getContext("2d");
-	const movementService = new MovementService();
-	const drawService = new DrawService(gameContext, squareWidth);
-
-	const maze = new Maze(initConfig, movementService, drawService);
-	const mazeData = maze.build();
-	const wanderer = new Wanderer(mazeData, movementService, drawService);
-	document.addEventListener('keyup', (e) => {
-		if (e.code === 'ArrowDown') {
-			wanderer.moveDown();
-		}
-		if (e.code === 'ArrowUp') {
-			wanderer.moveUp();
-		}
-		if (e.code === 'ArrowRight') {
-			wanderer.moveRight();
-		}
-		if (e.code === 'ArrowLeft') {
-			wanderer.moveLeft();
-		}
-	})
 </script>
 
 <main>
