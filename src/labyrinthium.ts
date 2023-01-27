2c3a8ee646168ffd1bc317f35d0f4f1d12763848
n-ckr-ch-rds-n <nicholasjohnrichardson@gmail.com>
2023-01-27 11:12:03 +0000

    Set controls with rxjs

--- src/labyrinthium.ts
+++ src/labyrinthium.ts
@@ -3,13 +3,25 @@ import { DrawService } from "./draw.service";
 import type { InitConfig } from "./init.config";
 import { Maze } from "./maze";
 import { Wanderer } from "./wanderer";
+import { fromEvent, Observable, throttleTime, filter, map, Subscription } from 'rxjs';
+import { ControlKey } from "./control.key";
 
 export class Labyrinthium {
+
+    private keyEventObservable: Observable<ControlKey>;
+    private controlSubscription: Subscription;
+
+    private movementByKey: Record<ControlKey, (w: Wanderer) => void> = {
+        [ControlKey.ArrowDown]: w => w.moveDown(),
+        [ControlKey.ArrowLeft]: w => w.moveLeft(),
+        [ControlKey.ArrowRight]: w => w.moveRight(),
+        [ControlKey.ArrowUp]: w => w.moveUp()
+    }
     
     constructor(private gameContext: CanvasRenderingContext2D,
         private config: InitConfig) {}
 
-    init() {
+    init(): void {
         const movementService = new MovementService();
         const drawService = new DrawService(this.gameContext, this.config.squareWidth);
         const maze = new Maze(this.config, movementService, drawService);
@@ -19,19 +31,24 @@ export class Labyrinthium {
     }
 
     private initialiseControls(wanderer: Wanderer) {
-        document.addEventListener('keyup', (e) => {
-            if (e.code === 'ArrowDown') {
-                wanderer.moveDown();
-            }
-            if (e.code === 'ArrowUp') {
-                wanderer.moveUp();
-            }
-            if (e.code === 'ArrowRight') {
-                wanderer.moveRight();
-            }
-            if (e.code === 'ArrowLeft') {
-                wanderer.moveLeft();
-            }
+        this.keyEventObservable = this.generateObservable();
+        this.controlSubscription = this.toKeySubscription(wanderer);
+    }
+
+    private generateObservable(): Observable<ControlKey> {
+        return this.keyEventObservable || fromEvent(document, 'keydown').pipe(
+            throttleTime(100),
+            filter((e: KeyboardEvent) => Object.values(ControlKey).includes(e.code as ControlKey)),
+            map((e: KeyboardEvent) => e.code as ControlKey)
+        );
+    }
+
+    private toKeySubscription(wanderer: Wanderer): Subscription {
+        if (this.controlSubscription) {
+            this.controlSubscription.unsubscribe();
+        }
+        return this.keyEventObservable.subscribe((key: ControlKey) => {
+            this.movementByKey[key](wanderer)
         })
     }
 }
